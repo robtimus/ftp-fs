1e8e477e0147483f6d1933833ba8195b4aa314bd
Rob Spoor <rob@icemanx.nl>
2022-12-10 15:13:44 +0100

    Use TestLogger JUnit extension

--- src/test/java/com/github/robtimus/filesystems/ftp/FTPFileSystemLoggingTest.java
+++ src/test/java/com/github/robtimus/filesystems/ftp/FTPFileSystemLoggingTest.java
@@ -27,8 +27,8 @@ import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import java.io.IOException;
 import java.net.URI;
@@ -37,86 +37,32 @@ import java.nio.file.FileSystems;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.log4j.Appender;
 import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.log4j.spi.LoggingEvent;
-import org.apache.log4j.varia.NullAppender;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import com.github.robtimus.junit.support.extension.testlogger.Reload4jLoggerContext;
+import com.github.robtimus.junit.support.extension.testlogger.TestLogger;
 
 @SuppressWarnings("nls")
 class FTPFileSystemLoggingTest extends AbstractFTPFileSystemTest {
-
-    private static Logger logger;
-    private static Level originalLevel;
-    private static List<Appender> originalAppenders;
-    private static List<Appender> originalRootAppenders;
-
-    private Appender appender;
 
     FTPFileSystemLoggingTest() {
         // not going to use any default FTP file system, so FTPFile strategy factory doesn't matter
         super(true, null);
     }
 
-    @BeforeAll
-    static void setupLogging() {
-        logger = LogManager.getLogger(FTPClientPool.class);
-        originalLevel = logger.getLevel();
-        originalAppenders = getAllAppenders(logger);
-        logger.removeAllAppenders();
-
-        Logger root = LogManager.getRootLogger();
-        originalRootAppenders = getAllAppenders(root);
-        root.removeAllAppenders();
-    }
-
-    private static List<Appender> getAllAppenders(Logger l) {
-        List<Appender> appenders = new ArrayList<>();
-        for (@SuppressWarnings("unchecked") Enumeration<Appender> e = l.getAllAppenders(); e.hasMoreElements(); ) {
-            appenders.add(e.nextElement());
-        }
-        return appenders;
-    }
-
-    @AfterAll
-    static void clearLogging() {
-        logger.setLevel(originalLevel);
-        for (Appender appender : originalAppenders) {
-            logger.addAppender(appender);
-        }
-
-        Logger root = LogManager.getRootLogger();
-        for (Appender appender : originalRootAppenders) {
-            root.addAppender(appender);
-        }
-    }
-
-    @BeforeEach
-    void setupAppender() {
-        appender = spy(new NullAppender());
-        logger.addAppender(appender);
-    }
-
-    @AfterEach
-    void clearAppender() {
-        logger.removeAppender(appender);
-    }
-
     @Test
-    void testLogging() throws IOException {
-        logger.setLevel(Level.TRACE);
+    void testLogging(@TestLogger.ForClass(FTPClientPool.class) Reload4jLoggerContext logger) throws IOException {
+        Appender appender = mock(Appender.class);
+        logger.setLevel(Level.TRACE)
+                .setAppender(appender)
+                .useParentAppenders(false);
 
         URI uri = getURI();
         try (FileSystem fs = FileSystems.newFileSystem(uri, createEnv(UNIX))) {
@@ -158,33 +104,36 @@ class FTPFileSystemLoggingTest extends AbstractFTPFileSystemTest {
     }
 
     @Test
-    void testCommandListener() throws IOException {
+    void testCommandListener(@TestLogger.ForClass(FTPClientPool.class) Reload4jLoggerContext logger) throws IOException {
+        Appender appender = mock(Appender.class);
+        logger.setAppender(appender)
+                .useParentAppenders(false);
 
         URI uri = getURI();
         try (FileSystem fs = FileSystems.newFileSystem(uri, createEnv(UNIX))) {
             logger.setLevel(Level.TRACE);
             reset(appender);
             FTPFileSystemProvider.keepAlive(fs);
-            assertKeepAliveLogged();
+            assertKeepAliveLogged(appender);
 
             // still trace level
             reset(appender);
             FTPFileSystemProvider.keepAlive(fs);
-            assertKeepAliveLogged();
+            assertKeepAliveLogged(appender);
 
             logger.setLevel(Level.DEBUG);
             reset(appender);
             FTPFileSystemProvider.keepAlive(fs);
-            assertKeepAliveNotLogged();
+            assertKeepAliveNotLogged(appender);
 
             // still debug level
             reset(appender);
             FTPFileSystemProvider.keepAlive(fs);
-            assertKeepAliveNotLogged();
+            assertKeepAliveNotLogged(appender);
         }
     }
 
-    private void assertKeepAliveLogged() {
+    private void assertKeepAliveLogged(Appender appender) {
         ArgumentCaptor<LoggingEvent> captor = ArgumentCaptor.forClass(LoggingEvent.class);
         verify(appender, atLeast(1)).doAppend(captor.capture());
 
@@ -201,7 +150,7 @@ class FTPFileSystemLoggingTest extends AbstractFTPFileSystemTest {
         ));
     }
 
-    private void assertKeepAliveNotLogged() {
+    private void assertKeepAliveNotLogged(Appender appender) {
         ArgumentCaptor<LoggingEvent> captor = ArgumentCaptor.forClass(LoggingEvent.class);
         verify(appender, atLeast(1)).doAppend(captor.capture());
 
